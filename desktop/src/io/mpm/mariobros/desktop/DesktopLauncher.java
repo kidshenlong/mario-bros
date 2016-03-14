2d39236ad20863c3f5c283c1f9f2b86d0ebe383b
kidshenlong <michaelpm91@googlemail.com>
2016-03-14 15:41:03 +0000

    Initial commit. Setup for LibGDX.

---
+++ desktop/src/io/mpm/mariobros/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.mpm.mariobros.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.mpm.mariobros.MarioBros;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MarioBros(), config);
+	}
+}
